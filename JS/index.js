88827607b99af73d000cf95ff06a1093fa0f9f4b
Gabriel Massensini <89725700+Massensini@users.noreply.github.com>
2022-09-01 09:13:33 -0300

    finished

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,6 @@
 let countSec = document.getElementById('countSec')
 let countMin = document.getElementById('countMin')
 let countHour = document.getElementById('countHour')
-let countDay = document.getElementById('countDay')
 
 const startBtn = document.getElementById('startBtn')
 const pauseBtn = document.getElementById('pauseBtn')
@@ -11,7 +10,6 @@ const clearBtn = document.getElementById('clearBtn')
 let ss = 0
 let mm = 0
 let hh = 0
-let dd = 0
 let startTimer;
 
 
@@ -27,12 +25,10 @@ function clear() {
     ss = 0
     mm = 0
     hh = 0
-    dd = 0
 
     countSec.innerHTML = '00'
     countMin.innerHTML = '00'
     countHour.innerHTML = '00'
-    countDay.innerHTML = '00'
 
     clearInterval(startTimer)
 }
@@ -46,16 +42,12 @@ function timer() {
     } else if (mm == 60) {
         mm = 0
         hh++
-    } else if (hh == 60) {
-        hh = 0
-        dd++
     }
 
 
     countSec.innerHTML = ss < 10 ? '0' + ss : ss
     countMin.innerHTML = mm < 10 ? '0' + mm : mm
     countHour.innerHTML = hh < 10 ? '0' + hh : hh
-    countDay.innerHTML = dd < 10 ? '0' + dd : dd
 }
 
 startBtn.addEventListener('click', start)
